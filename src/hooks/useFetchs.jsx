c30dbdf6d73bd3acd33a95d2419612374c8f6863
ckdgus222 <ckdgus1308@naver.com>
2024-05-21 14:43:33 +0900

    끝

--- src/hooks/useFetchs.jsx
+++ src/hooks/useFetchs.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-// const exAPI = "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=Bjr95JzquH4EmHhc5M9q7jLqK6yxT8HS&searchdate=20240506&data=AP01";
 const useFetch = (url) => {
   const [data, setData] = useState([]);
 
