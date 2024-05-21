460f779a604837855939b2ad5690c24830cd4566
ckdgus222 <ckdgus1308@naver.com>
2024-05-21 16:14:50 +0900

    CORS

--- src/util/exchangeAPI.js
+++ src/util/exchangeAPI.js
@@ -5,7 +5,7 @@ export const exchangeAPI = () => {
   const day = String(now.getDate()).padStart(2, "0");
 
   const API_KEY = String(import.meta.env.VITE_API_KEY);
-  const API_SEARCHDATE = `${year}${month}${day}`;
+  const API_SEARCHDATE = String(`${year}${month}${day}`);
   const dataType = String("AP01");
 
   const DATA_API = `/api?authkey=${API_KEY}&searchdate=${API_SEARCHDATE}&data=${dataType}`;
