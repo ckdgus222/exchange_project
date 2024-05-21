a18416d9678c70be8179f694c0f35760f80d8876
ckdgus222 <ckdgus1308@naver.com>
2024-05-21 17:40:08 +0900

    cors

--- src/components/Exchange.jsx
+++ src/components/Exchange.jsx
@@ -18,7 +18,7 @@ const Exchange = ({ changeState }) => {
   const [krwAmount, setKrwAmount] = useState(0);
   const [currentTime, setCurrentTime] = useState("");
 
-  const data = useFetch(data_API);
+  const data = useFetch(import.meta.env.VITE_API_URL);
 
   useEffect(() => {
     const timer = setInterval(() => {
