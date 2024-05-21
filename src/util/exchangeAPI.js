c306c7049367fe1aa57cab6af165d5213f19bc47
ckdgus222 <ckdgus1308@naver.com>
2024-05-21 15:02:45 +0900

    KEY

--- src/util/exchangeAPI.js
+++ src/util/exchangeAPI.js
@@ -1,11 +1,10 @@
-import { config } from "./apikey.js";
 export const exchangeAPI = () => {
   const now = new Date('2024-05-03');
   const year = now.getFullYear();
   const month = String(now.getMonth() + 1).padStart(2, "0");
   const day = String(now.getDate()).padStart(2, "0");
 
-  const API_KEY = String(config.EXCHANGE_KEY);
+  const API_KEY = String(import.meta.env.VITE_API_KEY);
   const API_SEARCHDATE = `${year}${month}${day}`;
   const dataType = String("AP01");
 
