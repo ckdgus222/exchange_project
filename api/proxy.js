2b4a07345364631c81104974af08052c77fc9b83
ckdgus222 <ckdgus1308@naver.com>
2024-06-19 16:02:51 +0900

    프록시

--- api/proxy.js
+++ api/proxy.js
@@ -1,27 +1,23 @@
 import fetch from 'node-fetch';
-import https from 'https';
 
 export default async function handler(req, res) {
   const { authkey, searchdate, data } = req.query;
-  
+
   if (!authkey || !searchdate || !data) {
     return res.status(400).json({ message: 'Missing required query parameters.' });
   }
 
   const apiUrl = `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${authkey}&searchdate=${searchdate}&data=${data}`;
 
-  const agent = new https.Agent({
-    rejectUnauthorized: false
-  });
-
   try {
-    const apiResponse = await fetch(apiUrl, { agent });
-    if (!apiResponse.ok) {
-      throw new Error(`API responded with status ${apiResponse.status}`);
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(`API responded with status ${response.status}`);
     }
 
-    const responseData = await apiResponse.json();
-    res.status(200).json(responseData);
+    res.status(200).json(data);
   } catch (error) {
     console.error('Error fetching data:', error);
     res.status(500).json({
@@ -30,5 +26,3 @@ export default async function handler(req, res) {
     });
   }
 }
-
-
