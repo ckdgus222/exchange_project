4e6421bffb2599126fbd2e17d3fd47397836730c
ckdgus222 <ckdgus1308@naver.com>
2024-06-19 21:20:52 +0900

    리디렉션

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,24 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
+import https from 'https';
+
+// SSL 인증서 검증을 비활성화하기 위한 HTTPS 에이전트 생성
+const agent = new https.Agent({
+  rejectUnauthorized: false
+});
 
 export default defineConfig({
   plugins: [react(), svgr(), tsconfigPaths()],
   server: {
     proxy: {
-      '/api/proxy': {  // 더 명확한 경로 매칭
-        target: 'https://www.koreaexim.go.kr/site/program/financial/exchangeJSON',
-        changeOrigin: true,
-        secure: false,
-        pathRewrite: { '^/api/proxy': '' },  // pathRewrite 옵션 사용
+      "/api/proxy": {  // '/api'가 아니라 '/api/proxy' 경로에 명시적으로 프록시를 설정
+        target: "https://www.koreaexim.go.kr",
+        changeOrigin: true, // Needed to handle CORS and origin headers
+        secure: false, // SSL 인증서 검증 비활성화
+        rewrite: path => path.replace(/^\/api\/proxy/, '/site/program/financial/exchangeJSON'), // 경로 재작성
+        agent // HTTPS 에이전트 설정
       },
     },
   },
