8d89a9639bda2e295584a575db7624846061c78a
ckdgus222 <ckdgus1308@naver.com>
2024-06-19 23:19:33 +0900

    다시

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,18 @@
-import axios from 'axios';
-import https from 'https';
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-export default async function handler(req, res) {
-  const { authkey, searchdate, data } = req.query;
-  if (!authkey || !searchdate || !data) {
-    return res.status(400).json({ message: 'Missing required query parameters.' });
-  }
-
-  const apiUrl = `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${authkey}&searchdate=${searchdate}&data=${data}`;
-  const agent = new https.Agent({
-    rejectUnauthorized: false
-  });
-
-  try {
-    // 리디렉션 자동 처리 활성화
-    let response = await axios.get(apiUrl, {
-      httpsAgent: agent,
-      maxRedirects: 5  // 리디렉션 최대 횟수 설정
-    });
-
-    // 최종 응답 검사
-    if (response.status !== 200) {
-      throw new Error(`API responded with status ${response.status}: ${response.statusText}`);
-    }
-
-    // 데이터 반환
-    res.status(200).json(response.data);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    res.status(500).json({ message: 'Unable to fetch data', error: error.toString() });
-  }
-}
+export default defineConfig({
+  plugins: [react(), svgr(), tsconfigPaths()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://www.koreaexim.go.kr",
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api\/proxy/, "/site/program/financial/exchangeJSON")
+      },
+    },
+  },
+});
