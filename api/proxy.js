479228d7fe9f986ccda61ee8b44a852e87fa3f52
ckdgus222 <ckdgus1308@naver.com>
2024-06-20 20:48:37 +0900

    det

--- api/proxy.js
+++ api/proxy.js
@@ -16,17 +16,18 @@ export default async function handler(req, res) {
   try {
     let response = await axios.get(apiUrl, {
       httpsAgent: agent,
-      maxRedirects: 0 // 리디렉션 자동 처리 비활성화
+      maxRedirects: 0 
     });
 
-    // 리디렉션 수동 처리
-    const visitedUrls = new Set(); // 방문한 URL 기록
-    while (response.status === 302 && response.headers.location) {
-      if (visitedUrls.has(response.headers.location)) {
-        throw new Error('Detected redirect loop'); // 무한 리디렉션 방지
-      }
-      visitedUrls.add(response.headers.location);
+    const visitedUrls = new Set();
+    visitedUrls.add(apiUrl);
 
+    while (response.status === 302 && response.headers.location) {
+      if (visitedUrls.has(response.headers.location) || response.headers.location === apiUrl) {
+        throw new Error('Detected redirect loop');
+      }
+
+      visitedUrls.add(response.headers.location);
       response = await axios.get(response.headers.location, {
         httpsAgent: agent,
         maxRedirects: 0
@@ -40,10 +41,6 @@ export default async function handler(req, res) {
     res.status(200).json(response.data);
   } catch (error) {
     console.error('Error fetching data:', error);
-    if (error.response) {
-      res.status(error.response.status).json({ message: 'Error from external API', details: error.response.data });
-    } else {
-      res.status(500).json({ message: 'Unable to fetch data', error: error.message });
-    }
+    res.status(500).json({ message: 'Unable to fetch data', error: error.message });
   }
 }
